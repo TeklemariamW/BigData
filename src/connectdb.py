f17e2655e51f4483791fe17ba484e37633580633
Teklemariam Weldehawariat <tekaliklfe@gmail.com>
2024-06-04 21:57:46 -0400

    connect mysql db using python with sqlalchemy

--- src/connectdb.py
+++ src/connectdb.py
@@ -1,15 +1,18 @@
+"""
+Connecting MySQL database using Python + SQLAlchemy remotely
+"""
 import pandas as pd
 from sqlalchemy import create_engine
 
 connection_string = 'mysql+mysqlconnector://root:root@localhost:3306/datacamp'
-sql_engine = create_engine(connection_string, echo=True)
+sql_engine = create_engine(connection_string)
 #connection = sql_engine.connect()
 
-#query = "select * from customers"
-query = "Create table Students(Id int, firstname varchar(20),lastname varchar(30), country varchar(30),number_of_rooms int);"
+# Creating table "Student" only once
+#query = "Create table Students(Id int, firstname varchar(20),lastname varchar(30), country varchar(30),number_of_rooms int);"
 
+query = "select * from airbnb_listings limit 3"
 df = pd.read_sql_query(query, sql_engine)
-
-#print(df)
+print(df)
 
 
